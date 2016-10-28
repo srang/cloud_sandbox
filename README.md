781f26d22ba330ae147cb874145e89fd0bd47283
Sam Rang <srang2010@gmail.com>
2016-10-28 16:49:37 -0500

    stuff

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # cloud_sandbox
 Sandbox for configuring apps with ansible
+
+```
+$ vagrant up # creates boxes
+$ ./site.yml # configures master and sets up routing between boxes
+$ ssh -A vagrant@master
+$ cd /opt/ansible
+$ ./local.yml # sets up app and web servers
--- files/fetch.js
+++ files/fetch.js
@@ -1,4 +1,13 @@
 $("#name-submit").on('click', function() {
     var name = $('#name-box').val();
     console.log("submitting " + name);
+    $.get({
+      url: 'app.server.local/'+name,
+      success: function() {
+        $('#name-last').text(name);
+      },
+      error: function() {
+        $('#name-last').text("error");
+      }
+    });
 });
