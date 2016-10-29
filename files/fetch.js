8495c5bbfe09d2ce0f4f2189d14809429bdbfa21
Sam Rang <srang2010@gmail.com>
2016-10-29 15:04:48 -0500

    use proxy

--- files/fetch.js
+++ files/fetch.js
@@ -2,7 +2,7 @@ $("#name-submit").on('click', function() {
     var name = $('#name-box').val();
     console.log("submitting " + name);
     $.get({
-      url: 'http://app.server.local/hello/'+name,
+      url: '/api/hello/'+name,
       success: function(resp) {
         $('#name-last').text(resp.body);
       },
